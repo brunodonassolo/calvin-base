ca26f14e2260b388a746a8f72d07b1a011d0fbf8
Bruno Donassolo <1733673+brunodonassolo@users.noreply.github.com>
2017-10-19 17:36:49 +0200

    Remove monitoring by now

--- extras/docker/g5k/calvin_startup.sh
+++ extras/docker/g5k/calvin_startup.sh
@@ -23,26 +23,3 @@ if [ $status -ne 0 ]; then
   echo "Failed to start csruntime: $status"
   exit $status
 fi
-
-
-# Naive check runs checks once a minute to see if either of the processes exited.
-# This illustrates part of the heavy lifting you need to do if you want to run
-# more than one service in a container. The container will exit with an error
-# if it detects that either of the processes has exited.
-# Otherwise it will loop forever, waking up every 60 seconds
-  
-while /bin/true; do
-  ps aux |grep netdata |grep -q -v grep
-  PROCESS_1_STATUS=$?
-  ps aux |grep blackbox_exporter |grep -q -v grep
-  PROCESS_2_STATUS=$?
-  ps aux |grep csruntime |grep -q -v grep
-  PROCESS_3_STATUS=$?
-  # If the greps above find anything, they will exit with 0 status
-  # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 ]; then
-    echo "One of the processes has already exited."
-    exit -1
-  fi
-  sleep 60
-done
