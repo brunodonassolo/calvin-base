b83d18f1b4fe01eede00de16d69a16b4f4098850
Bruno Donassolo <1733673+brunodonassolo@users.noreply.github.com>
2017-10-25 15:56:01 +0200

    Fix

--- extras/docker/g5k/calvin_startup.sh
+++ extras/docker/g5k/calvin_startup.sh
@@ -22,11 +22,11 @@ then
 fi
 if [ -z "$CALVIN_ATTRIBUTES" ];
 then
-    CALVIN_ATTRIBUTES='{}'
+    CALVIN_ATTRIBUTES={}
 fi
 if [ -z "$CALVIN_EXTERNAL_IP" -o -z "$CALVIN_EXTERNAL_CONTROL_IP" ];
 then
-    csruntime -n $CALVIN_IP -p $CALVIN_PORT -c $CALVIN_CONTROL_PORT --attr $CALVIN_ATTRIBUTES --loglevel=INFO &
+    csruntime -n $CALVIN_IP -p $CALVIN_PORT -c $CALVIN_CONTROL_PORT --attr "$CALVIN_ATTRIBUTES" --loglevel=INFO &
 else
     csruntime -n $CALVIN_IP --external $CALVIN_EXTERNAL_IP --external-control $CALVIN_EXTERNAL_CONTROL_IP -p $CALVIN_PORT -c $CALVIN_CONTROL_PORT --attr $CALVIN_ATTRIBUTES --loglevel=INFO &
 fi
